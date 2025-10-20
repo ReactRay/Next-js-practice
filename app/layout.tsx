4f5afae9211786c37179e8732b5b2c466bc7b892
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-20 15:56:58 +0300

    first client compnent

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 import './globals.css';
 import NavBar from '@/components/NavBar';
+import { Metadata } from 'next';
 import { Inter, Inconsolata, Roboto } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 const inconsolata = Inconsolata({ subsets: ['latin'] });
 const roboto = Roboto({ subsets: ['latin'], weight: ['400'] })
+
+
+export const metadata: Metadata = {
+  title: 'RM NEXT-JS',
+  description: 'my first next-js project',
+  keywords: 'Next-js, typescript, tailwindcss'
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return <html lang='eng'>
