bb9b6afefaa4973060073fd2b47eb2b9897f896c
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-20 16:20:34 +0300

    server component

---
+++ app/tours/loading.tsx
@@ -0,0 +1,15 @@
+
+"use client"
+
+import React from 'react'
+
+
+function loading() {
+    return (
+        <span className='text-xl capitalize'>
+            loading...
+        </span>
+    )
+}
+
+export default loading
