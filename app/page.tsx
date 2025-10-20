8fa071b2c601bc4d936ca5ea27f50aec82256d21
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-20 15:27:00 +0300

    routing in next

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
+      <Link href={'/about'} className="text-xl text-blue-500  inline-block border-2 p-1 ">about page</Link>
     </div>
   );
 }
