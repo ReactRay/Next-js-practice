78ea7e345bca40226b0c70305d0d760e3927d689
Radwan Mansur <raydiaz1899@gmail.com>
2025-11-03 01:51:23 +0200

    going back to this project

--- app/info/contact/page.tsx
+++ app/info/contact/page.tsx
@@ -1,11 +1,15 @@
 
 
 import React from 'react'
+import Link from 'next/link'
 
 function Contact() {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className='  align-center gap-10'>
+            <h1 className='text-3xl text-gray-600'>Contact page</h1>
+            <Link href='/' className='text-xl text-blue-500 inline-block '>
+                back home
+            </Link>
         </div>
     )
 }
