43574cd138a04042124db4581645ca45ea65ef1d
Radwan Mansur <raydiaz1899@gmail.com>
2025-11-04 02:25:10 +0200

    learning about server actions

---
+++ app/actions/page.tsx
@@ -0,0 +1,16 @@
+
+
+import UserList from '@/components/UserList'
+import Form from '@/components/Form'
+import React from 'react'
+
+function ActionsPage() {
+    return (
+        <div>
+            <Form />
+            <UserList />
+        </div>
+    )
+}
+
+export default ActionsPage
