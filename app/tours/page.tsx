d5f9a1034b492dbae065dc61820dc6edfb6ac8f3
Radwan Mansur <raydiaz1899@gmail.com>
2025-11-04 02:15:19 +0200

    update  routes

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-
+import Image from 'next/image'
 type Tour = {
     id: string;
     name: string;
@@ -14,10 +14,11 @@ const url = 'https://www.course-api.com/react-tours-project'
 
 const fetchTours = async (): Promise<Tour[]> => {
     await new Promise((resolve) => {
-        setTimeout(resolve, 2000)
+        setTimeout(resolve, 1000)
     })
     const response = await fetch(url);
     const data: Tour[] = await response.json()
+    console.log(data)
     return data
 }
 
@@ -34,6 +35,13 @@ async function ToursPage() {
                     return <Link
                         className='hover:text-blue-500'
                         href={`/tours/${tour.id}`} key={tour.id}>
+                        <div className='relative h-48 mb-2'>
+                            <Image src={tour.image} alt={tour.name} fill priority
+                                sizes='(max-width: 768px) 10vw, (max-width: 1200px) 50vw, 33vw'
+
+                                className='transition duration-1000 object-cover rounded shadow-2xs 
+                                hover:shadow-2xl' />
+                        </div>
                         <h2>{tour.name}</h2>
                     </Link>
                 })}
